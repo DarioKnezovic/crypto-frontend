f100e596b8c59d884e6e199de71dc97d6212bc11
DarioKnezovic <dknezovic997@gmail.com>
2022-07-29 18:58:28 +0200

    Intoduced new select input.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,15 +1,100 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import "./Select.css";
 
 const Select = (props) => {
+    const [isOptionsOpen, setIsOptionsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(0);
+
+    const optionsList = [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5"
+    ];
+
+    const toggleOptions = () => {
+        setIsOptionsOpen(!isOptionsOpen);
+    };
+
+    const setSelectedThenCloseDropdown = (index) => {
+        setSelectedOption(index);
+        setIsOptionsOpen(false);
+    };
+
+    const handleKeyDown = (index) => (e) => {
+        switch (e.key) {
+            case " ":
+            case "SpaceBar":
+            case "Enter":
+                e.preventDefault();
+                setSelectedThenCloseDropdown(index);
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleListKeyDown = (e) => {
+        switch (e.key) {
+            case "Escape":
+                e.preventDefault();
+                setIsOptionsOpen(false);
+                break;
+            case "ArrowUp":
+                e.preventDefault();
+                setSelectedOption(
+                    selectedOption - 1 >= 0 ? selectedOption - 1 : optionsList.length - 1
+                );
+                break;
+            case "ArrowDown":
+                e.preventDefault();
+                setSelectedOption(
+                    selectedOption === optionsList.length - 1 ? 0 : selectedOption + 1
+                );
+                break;
+            default:
+                break;
+        }
+    };
+
     return (
         <div className="input-group">
             <label>Currency from</label>
-            <select>
-                <option>Bitcoin</option>
-                <option>Ethernum</option>
-            </select>
+            <button
+                id="button-select"
+                type="button"
+                aria-haspopup="listbox"
+                aria-expanded={isOptionsOpen}
+                className={isOptionsOpen ? "expanded" : ""}
+                onClick={toggleOptions}
+                onKeyDown={handleListKeyDown}
+            >
+                {optionsList[selectedOption]}
+            </button>
+            <ul
+                className={`options ${isOptionsOpen ? "show" : ""}`}
+                role="listbox"
+                aria-activedescendant={optionsList[selectedOption]}
+                tabIndex="0"
+                onKeyDown={handleListKeyDown}
+            >
+                {optionsList.map((option, index) => (
+                    <li
+                        id={option}
+                        role="option"
+                        aria-selected={selectedOption === index}
+                        tabIndex={0}
+                        onKeyDown={handleKeyDown(index)}
+                        onClick={() => {
+                            setSelectedThenCloseDropdown(index);
+                        }}
+                    >
+                        {option}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
