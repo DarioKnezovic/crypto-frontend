4b5ffcc6ffc4dddde23e3007dcf28558200e9261
DarioKnezovic <dknezovic997@gmail.com>
2022-07-31 15:17:19 +0200

    Started to work on history module.

---
+++ src/components/History/History.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./History.css";
+
+const History = () => {
+    return (
+        <div className="history">
+            <h1 className="title">History</h1>
+        </div>
+    )
+}
+
+export default History;
